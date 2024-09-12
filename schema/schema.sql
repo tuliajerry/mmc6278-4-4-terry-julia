a5a17ce6e494ddf6ed7bdb6643698b4de9579140
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 15:15:33 -0400

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,33 +1,30 @@
 CREATE DATABASE IF NOT EXISTS music_db;
 
 
-USE music_db;
-
-
-CREATE TABLE IF NOT EXISTS artists (
+CREATE TABLE IF NOT EXISTS music_db.artists (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
 
-CREATE TABLE IF NOT EXISTS albums (
+CREATE TABLE IF NOT EXISTS music_db.albums (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   artist_id INT,
   release_year INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE
+  FOREIGN KEY (artist_id) REFERENCES music_db.artists(id) ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS songs (
+CREATE TABLE IF NOT EXISTS music_db.songs (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   track_number INT,
   artist_id INT,
   album_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE,
-  FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE
+  FOREIGN KEY (artist_id) REFERENCES music_db.artists(id) ON DELETE CASCADE,
+  FOREIGN KEY (album_id) REFERENCES music_db.albums(id) ON DELETE CASCADE
 );
