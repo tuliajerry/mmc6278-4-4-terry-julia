5bb0f4347b5e044ca5cc3fdb806e8d8b00bb2d69
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 14:15:32 -0400

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -4,29 +4,24 @@ USE music_db;
 
 CREATE TABLE artists (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100)  
+  name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE albums (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100),       
-  artist_id INT,           
-  release_year INT,         
-  FOREIGN KEY (artist_id)
-    REFERENCES artists (id)
-    ON DELETE CASCADE
+  name VARCHAR(100) NOT NULL,
+  artist_id INT,
+  release_year INT,
+  FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE
 );
 
 CREATE TABLE songs (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100),       
-  track_number INT,        
-  artist_id INT,           
-  album_id INT,             
-  FOREIGN KEY (artist_id)
-    REFERENCES artists (id)
-    ON DELETE CASCADE,
-  FOREIGN KEY (album_id)
-    REFERENCES albums (id)
-    ON DELETE CASCADE
+  name VARCHAR(100) NOT NULL,
+  track_number INT NOT NULL,
+  artist_id INT,
+  album_id INT,
+  FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE CASCADE,
+  FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE
 );
+
