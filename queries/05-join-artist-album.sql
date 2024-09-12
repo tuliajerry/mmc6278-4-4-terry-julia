3f86833a257993353b14ad1b03529c331add46b4
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 13:45:24 -0400

    Update 05-join-artist-album.sql

--- queries/05-join-artist-album.sql
+++ queries/05-join-artist-album.sql
@@ -1,3 +1,14 @@
 -- Join artists on albums, pulling artists.name as artist_name
 -- albums.name as album_name and albums.release_year
 -- sort by artist name and album name ascending
+SELECT 
+    artists.name AS artist_name,
+    albums.name AS album_name,
+    albums.release_year
+FROM 
+    albums
+INNER JOIN 
+    artists ON albums.artist_id = artists.id
+ORDER BY 
+    artist_name ASC, 
+    album_name ASC;
