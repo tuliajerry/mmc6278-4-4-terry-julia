140ac677f343776fc5bb1c680c263745fb4c27f8
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 16:37:18 -0400

    Update 06-insert.sql

--- queries/06-insert.sql
+++ queries/06-insert.sql
@@ -1,4 +1,11 @@
 -- Insert Track #13 titled "Endless, Nameless" into Nirvana's Nevermind album
 -- Nirvana's id is 6, Nevermind's id is 9
 INSERT INTO songs (name, track_number, artist_id, album_id)
-VALUES ('Endless, Nameless', 13, 6, 9);
+SELECT 'Endless, Nameless', 13, 6, 9
+FROM DUAL
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM songs
+    WHERE name = 'Endless, Nameless'
+      AND album_id = 9
+);
