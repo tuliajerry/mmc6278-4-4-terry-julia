5a10d41b97eae32acfdb9dcac362ad24e50c5044
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 13:47:57 -0400

    Update 08-count-albums-by-artist.sql

--- queries/08-count-albums-by-artist.sql
+++ queries/08-count-albums-by-artist.sql
@@ -6,6 +6,9 @@
 SELECT
   artists.name,
   COUNT(albums.id) AS number_of_albums
-  FROM albums
-  INNER JOIN artists ON artists.id=albums.artist_id
-  GROUP BY artists.id;
+FROM
+  albums
+INNER JOIN
+  artists ON artists.id = albums.artist_id
+GROUP BY
+  artists.id, artists.name;
