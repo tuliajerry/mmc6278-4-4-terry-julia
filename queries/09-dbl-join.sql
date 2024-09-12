916c0b8e0ae58ebdf7b8079f9c13a143a2c136d4
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 14:27:36 -0400

    Update 09-dbl-join.sql

--- queries/09-dbl-join.sql
+++ queries/09-dbl-join.sql
@@ -7,19 +7,15 @@
 -- Then sort by albums.release_year and songs.track_number ascending
 
 SELECT
-    artists.name AS artist_name,
-    albums.name AS album_name,
-    songs.name AS track_title,
+    artists.name as artist_name,
+    albums.name as album_name,
+    songs.name as track_title,
     albums.release_year,
     songs.track_number
-FROM
-    artists
-INNER JOIN
-    songs ON songs.artist_id = artists.id
-INNER JOIN
-    albums ON songs.album_id = albums.id
-WHERE
-    artists.name = 'Nirvana'
-ORDER BY
-    albums.release_year ASC,
-    songs.track_number ASC;
+  FROM artists
+  INNER JOIN songs ON
+    songs.artist_id=artists.id
+  INNER JOIN albums ON
+    songs.album_id=albums.id
+  WHERE artists.name="Nirvana"
+  ORDER BY albums.release_year, songs.track_number ASC;
